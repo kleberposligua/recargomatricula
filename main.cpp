7b47d6279a9fd86841867bdefd60f6bcd0f2befe
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2021-06-17 11:57:13 -0500

    Enunciado del ejercicio

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//Se ingresa la edad de un estudiante. Si la edad es de 5 a 10 años, se hace
+//un recargo del 1% al costo de la matrícula. Si la edad es superior a 10 años
+//y menor o igual que 15 años, el recargo será del 2%. Si la edad es mayor que
+//15 y menor o igual que 20, el recargo es del 4%. Si la edad es superior
+//a 20 años, el recargo es del 6%.
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
